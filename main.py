2608f787e9fba50219aba07313f6febe5c8fe064
Stahux <sn@nizin.pl>
2021-02-27 15:48:14 +0100

    First test doing something, but totally improperly!

---
+++ main.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 26 18:55:44 2021
+
+@author: staszek
+"""
+
+
+
+
+
+
+
+
+
+
+
+#PLAN:
+#1. load kinetic
+#2. opt constant line of amps
+#3. start optimization with given alpha
+#4.  ....
