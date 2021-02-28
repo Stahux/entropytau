640dae83dbde56eef03ee92f6f13b083d05de652
Stahux <sn@nizin.pl>
2021-02-28 12:00:32 +0100

    Switched from lmfit to scipy minimize.

--- main.py
+++ main.py
@@ -6,9 +6,8 @@ Created on Fri Feb 26 18:55:44 2021
 @author: staszek
 """
 import numpy as np
-import scipy as sp
+from scipy import special, optimize
 import matplotlib.pyplot as plt
-import lmfit
 import copy
 
 
@@ -23,9 +22,10 @@ error = np.std(a_vect[np.where(t_vect <= -0.5)])
 t_vect = t_vect[20:]
 a_vect = a_vect[20:]
 
+
 def gaussExp(t, sigma, A, tau):
     eksp = np.exp(-(t - sigma**2 / (2 * tau))/tau)/2
-    erfu = sp.special.erf((t - sigma**2/tau)/(np.sqrt(2)*sigma)) + 1
+    erfu = special.erf((t - sigma**2/tau)/(np.sqrt(2)*sigma)) + 1
     return A * eksp * erfu
 
 def buildExpArray(t_vect, tauspace, t0, sigma):
@@ -44,58 +44,50 @@ exparray = buildExpArray(t_vect, tauspace, 0.0, 0.0001)
 
 
 
-def calculateKinetic(params):
-    p = np.array([x.value for x in params.values()])
-    
+def calculateKinetic(p):
     a_model_vect = exparray.dot(p)
-
     return a_model_vect
 
-def calcEntropy(params):
-    p = np.array([x.value for x in params.values()])
-    entvect = -np.abs(p)*np.log(np.abs(p))
+def calcEntropy(p):
+    entvect = np.abs(p) - np.abs(p)*np.log(np.abs(p)) #Skilling
     for i in range(entvect.shape[0]): #bad solution
         if(np.isnan(entvect[i])):
             entvect[i] = 0.0
-    return entvect
+    return np.sum(entvect)
     #problems:
     #minor: amps over 1 will give negative entropy
     #major: amps with zero will give nan
-    #very major: how to incorporate that to residual vector?
 
-def residualWithRegularisation(params):
-    values = calculateKinetic(params)
+
+def residualWithRegularisation(p):
+    values = calculateKinetic(p)
     residuals = (values - a_vect)/(error*values.shape[0])
+    entropy = calcEntropy(p)
+    chi2 = np.sum(np.power(residuals,2))
+    print("Entropy is: " + str(entropy) + ", and chi2 is: " + str(chi2))
+    return chi2 + alpha*entropy #should be with minus... it seems that minimizing etropy gives something, instead of maximizing. makes no sense at this point...
 
-    return np.append(residuals,alpha*calcEntropy(params)) #it can be done better i think
-
-def plotKinetic(params):
+def plotKinetic(p):
     plt.figure()
     plt.plot(t_vect, a_vect, "bo")
-    plt.plot(t_vect, calculateKinetic(params), "r-")
+    plt.plot(t_vect, calculateKinetic(p), "r-")
     plt.xscale(value = "log")        
     plt.show()        
     
-def plotDistribution(params):
-    p = np.array([x.value for x in params.values()])
+def plotDistribution(p):
     plt.figure()
     plt.plot(tauspace, p, "bo")  
     plt.xscale(value = "log")
     plt.show() 
 
 
-p = lmfit.Parameters()
+p = copy.deepcopy(tauspace)
 for i in range(tauspace.shape[0]):
-    p.add("A"+str(i), 0.00000001)
-    
-if(tauspace.shape[0] >= t_vect.shape[0]):
-    raise Exception("Space of taus has to be smaller that number of delays!")
+    p[i] = 0.00000001
 
-mini = lmfit.Minimizer(residualWithRegularisation, p, nan_policy='propagate')
-out = mini.minimize(method='leastsq')
-out_params = out.params
-lmfit.report_fit(out.params)
-print(out.chisqr)
+out_params = optimize.minimize(residualWithRegularisation, p)
 
-plotKinetic(out_params)
-plotDistribution(out_params)
+print(out_params)
+
+plotKinetic(out_params.x)
+plotDistribution(out_params.x)
